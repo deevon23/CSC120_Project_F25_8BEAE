7a83355c4e60a5b4e3a63caf1fe44383069cef0f
Dorian <dorianvaughan23@gmail.com>
2025-10-02 16:15:28 -0400

    Move file to Staged Changes

---
+++ main.py
@@ -0,0 +1,9 @@
+game_name = "Turbo"
+print("Welcome to " + game_name + "!")
+print("=================")
+
+# Ask for the character's name
+name = input("Before we begin, what is your character's name?\n> ")
+
+# Print the name
+print("Great, " + name + "! Let's begin the adventure!")
